e120abaa8a658130f839f661ff1e8130e2f08fc6
Code4Blessings <robincw35@gmail.com>
2021-03-28 02:12:21 -0400

    Implemented provider and reconfigured app accordingly.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import 'package:flutter_shopp_app/screens/product_detail.dart';
 import 'package:flutter_shopp_app/screens/products_overview.dart';
+import './providers/products.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,18 +13,21 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Shop App',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-        accentColor: Colors.green,
-        fontFamily: 'Lato'
-      ),
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'Shop App',
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+          accentColor: Colors.green,
+          fontFamily: 'Lato'
+        ),
 
-      home: ProductsOverview(),
-      routes: {
-        ProductDetail.routeName: (ctx) => ProductDetail()
-      },
+        home: ProductsOverview(),
+        routes: {
+          ProductDetail.routeName: (ctx) => ProductDetail(),
+        },
+      ),
     );
   }
 }
